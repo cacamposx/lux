bda475854826c1589689e981e72bc378877ad92d
Thiago Santana <thiago.silva10051.ts@gmail.com>
2024-09-25 23:16:24 -0300

    Adicionando botão de ativar e desativa status produto
    Adicionei o botão ao listar produto.
    Tirei o Listar Pedido da tela do Admin.

--- src/main/java/br/com/pi/lux/controller/AlterarStatusController.java
+++ src/main/java/br/com/pi/lux/controller/AlterarStatusController.java
@@ -1,6 +1,8 @@
 package br.com.pi.lux.controller;
 
+import br.com.pi.lux.model.Produto;
 import br.com.pi.lux.model.Usuario;
+import br.com.pi.lux.repository.ProdutoRepository;
 import br.com.pi.lux.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,6 +16,8 @@ public class AlterarStatusController {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+    @Autowired
+    private ProdutoRepository produtoRepository;
 
     @GetMapping("/alterarStatus")
     public String alterarStatus(@RequestParam("idUsuario") int idUsuario) {
@@ -24,5 +28,16 @@ public class AlterarStatusController {
             usuarioRepository.save(usuario);
         }
         return "redirect:/listarUsu";
+    }
+
+    @GetMapping("/alterarStatusProd")
+    public String alterarStatusProduto(@RequestParam("idProduto") int idProduto) {
+        Optional<Produto> produtoOptional = produtoRepository.findById(idProduto);
+        if (produtoOptional.isPresent()) {
+            Produto produto = produtoOptional.get();
+            produto.setStatus(!produto.isStatus());
+            produtoRepository.save(produto);
+        }
+        return "redirect:/listarProd";
     }
 }
--- src/main/java/br/com/pi/lux/repository/ProdutoRepository.java
+++ src/main/java/br/com/pi/lux/repository/ProdutoRepository.java
@@ -1,11 +1,16 @@
 package br.com.pi.lux.repository;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import br.com.pi.lux.model.Produto;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
     List<Produto> findAllByOrderByDataDesc();
+    Page<Produto> findByNomeContainingIgnoreCase(String nome, Pageable pageable);
 }
